2b20f04e63c21e4da229b3cea72cdd02e6aba258
mafora12 <marianaflorez1224@gmail.com>
2026-03-16 22:53:27 -0500

    ya

--- TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Parcial-2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c721f95612353456f9cacd974f696205e32a058")]
-[assembly: System.Reflection.AssemblyProductAttribute("Parcial-2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Parcial-2")]
+[assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
