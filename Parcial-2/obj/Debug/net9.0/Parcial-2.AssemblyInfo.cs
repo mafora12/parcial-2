d4825bd07529493c4df4e030706c521075348dc5
mafora12 <marianaflorez1224@gmail.com>
2026-03-16 23:16:48 -0500

    fin

--- Parcial-2/obj/Debug/net9.0/Parcial-2.AssemblyInfo.cs
+++ Parcial-2/obj/Debug/net9.0/Parcial-2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Parcial-2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c721f95612353456f9cacd974f696205e32a058")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f09e6a4bcd0d37e74844b06925754efb1530f657")]
 [assembly: System.Reflection.AssemblyProductAttribute("Parcial-2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Parcial-2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
